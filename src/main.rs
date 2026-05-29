024cbc7cc6825975c51593e58eb64473eb722e32
wiser-fz <wiserfz810@gmail.com>
2026-05-29 16:40:33 +0800

    Init translator project

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+
+use translator::parse_cli;
+use translators::{GoogleTranslator, Translator};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let cli = parse_cli();
+    let translator = GoogleTranslator::default();
+    let translated_text = translator
+        .translate_async(
+            &cli.input_text(),
+            &cli.source_language(),
+            &cli.target_language(),
+        )
+        .await?;
+
+    println!("{translated_text}");
+
+    Ok(())
+}
